4944f744c2d10088dbf9afe923fe497fc2a9cfcf
seanstory <skyloftproductions@Skylofts-iMac.hitronhub.home>
2022-01-31 15:17:27 -0800

    limited number of bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,16 +22,15 @@ class AlienInvasion:
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
 
-        #Set the background color.
-        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
+            
 
 
   
@@ -67,11 +66,17 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group."""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)    
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)    
 
-
-               
+    def _update_bullets(self):
+        '''Update position of bullets and get rid of old bullets'''
+        self.bullets.update()
+        # Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
 
                     
 
